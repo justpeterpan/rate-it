87f16422f24ccec389ef5fe716924e71b7797c51
justpeterpan <peter.busch@posteo.net>
2024-04-19 02:12:22 +0200

    feat: reimplement album search for new layout

---
+++ components/ColorSlider.vue
@@ -0,0 +1,70 @@
+<template>
+  <div
+    @click="$emit('pickColor', props.color)"
+    class="rounded-full min-w-12 h-12 transition-[width] duration-500 ease-in-out appearance-none"
+    :class="[
+      selectedColor === props.color
+        ? 'w-full bg-white'
+        : `w-12 ${bgColors[props.color]}`,
+    ]"
+  >
+    <input
+      v-if="selectedColor === `${props.color}`"
+      type="range"
+      min="0"
+      max="255"
+      v-model="colorRange"
+      class="rounded-full min-w-12 h-12 bg-gradient-to-r from-white transition-[width] duration-500 ease-in-out appearance-none"
+      :class="[
+        selectedColor === `${props.color}`
+            ? `w-full ${gradientColors[color as keyof typeof gradientColors]}`
+          : 'w-12',
+      ]"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = withDefaults(
+  defineProps<{
+    color?: string
+    selectedColor: string
+  }>(),
+  {
+    color: 'violet',
+  }
+)
+const emit = defineEmits(['pickColor'])
+const colorRange = defineModel()
+
+const gradientColors = {
+  green: 'to-green-300',
+  violet: 'to-violet-300',
+  lime: 'to-lime-300',
+  yellow: 'to-yellow-300',
+  blue: 'to-blue-300',
+  sky: 'to-sky-300',
+}
+
+const bgColors: Record<string, string> = {
+  green: 'bg-green-300',
+  violet: 'bg-violet-300',
+  lime: 'bg-lime-300',
+  yellow: 'bg-yellow-300',
+  blue: 'bg-blue-300',
+  sky: 'bg-sky-300',
+}
+</script>
+
+<style>
+input[type='range']::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 16px;
+  height: 16px;
+  border-radius: 100%;
+  background: rgba(255, 255, 255, 0.9);
+  border: 2px solid black;
+  cursor: pointer;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,18 +5,16 @@
     </button>
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-1">
       <NuxtLink to="configurator">
-        <TemplateMnml />
+        <TemplateMnml
+          :album="{
+            rating: 4,
+            artist: 'Burial',
+            title: 'Awakening',
+            date: '06.06.2022',
+            review: 'Masterpiece',
+          }"
+        />
       </NuxtLink>
-      <div
-        class="flex flex-col bg-white text-black p-20 rounded-xl cursor-pointer group relative hover:bg-green-300 transition duration-500 ease-in-out"
-      >
-        <h2>Template 2</h2>
-        <p>Some text here</p>
-      </div>
-      <div class="flex flex-col bg-white text-black p-4 rounded-xl">
-        <h2>Template 2</h2>
-        <p>Some text here</p>
-      </div>
     </div>
   </div>
 </template>
