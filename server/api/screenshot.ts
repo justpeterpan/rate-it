43eefeb255b3a278b5e10910029b69039ac77a15
justpeterpan <peter.busch@posteo.net>
2024-04-22 01:33:39 +0200

    fix: make screenshot endpoint functional again

--- server/api/screenshot.ts
+++ server/api/screenshot.ts
@@ -12,7 +12,7 @@ const runtimeConfig = useRuntimeConfig()
 const exePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
 
 const S3 = new S3Client({
-  region: 'auto',
+  region: 'weur',
   endpoint: runtimeConfig.cloudflare.endpoint,
   credentials: {
     accessKeyId: runtimeConfig.cloudflare.credentials.key,
@@ -20,8 +20,34 @@ const S3 = new S3Client({
   },
 })
 
+type QueryValue =
+  | string
+  | number
+  | undefined
+  | null
+  | boolean
+  | Array<QueryValue>
+  | Record<string, any>
+
+function isStringQuery(query: QueryValue | QueryValue[]): string | number {
+  if (!query) return ''
+  if (typeof query === 'string') return query
+  if (Array.isArray(query)) return query[0] ?? ''
+  return ''
+}
+
 export default defineEventHandler(async (event) => {
-  const { artist, album, date, cover, rating, notes, theme } = getQuery(event)
+  const {
+    album,
+    artist,
+    bgColor,
+    cover,
+    date,
+    opacity,
+    rating,
+    review,
+    template,
+  } = getQuery(event)
 
   console.log('taking screenshot')
   const browser = await puppeteer.launch({
@@ -32,22 +58,34 @@ export default defineEventHandler(async (event) => {
   const page = await browser.newPage()
   await page.setViewport({ width: 430, height: 932 })
   await page.goto(
-    `${runtimeConfig.url}/?artist=${encodeURIComponent(
-      artist
-    )}&album=${encodeURIComponent(album)}&date=${encodeURIComponent(
-      date
-    )}&cover=${encodeURIComponent(cover)}&rating=${encodeURIComponent(
-      rating
-    )}&notes=${encodeURIComponent(notes)}`,
+    `${runtimeConfig.url}/screenshot/?album=${encodeURIComponent(
+      isStringQuery(album)
+    )}&artist=${encodeURIComponent(
+      isStringQuery(artist)
+    )}&bgColor=${encodeURIComponent(
+      isStringQuery(bgColor)
+    )}&cover=${encodeURIComponent(
+      isStringQuery(cover)
+    )}&date=${encodeURIComponent(
+      isStringQuery(date)
+    )}&opacity=${encodeURIComponent(
+      isStringQuery(opacity)
+    )}&rating=${encodeURIComponent(
+      isStringQuery(rating)
+    )}&review=${encodeURIComponent(
+      isStringQuery(review)
+    )}&template=${encodeURIComponent(isStringQuery(template))}
+    `,
     { waitUntil: 'networkidle2' }
   )
   await page.emulateMediaFeatures([
-    { name: 'prefers-color-scheme', value: `${theme}` },
+    { name: 'prefers-color-scheme', value: 'light' },
   ])
   const fileNameWithTmp = `/tmp/${artist}-${album}-${new Date().toISOString()}.png`
   const fileNameWithoutTmp = `${artist}-${album}-${new Date().toISOString()}.png`
   const element = await page.waitForSelector('#screenshot')
-  await element.screenshot({ path: fileNameWithTmp })
+  // fix typescript warning
+  await element!.screenshot({ path: fileNameWithTmp })
   const fileStream = fs.readFileSync(fileNameWithTmp)
   const uploadParams: PutObjectCommandInput = {
     Bucket: 'rate',
